1338cb700c7067f7c9cc18792f2b0024e5781533
nburza <nicoburza_96@hotmail.com>
2020-05-14 16:27:46 -0300

    agrego subclase administrador

---
+++ src/main/java/Administrador.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public class Administrador extends Usuario{
+
+    public Administrador(String nombreUsuario, String contrasenia) {
+
+        super(nombreUsuario,contrasenia);
+    }
+}
