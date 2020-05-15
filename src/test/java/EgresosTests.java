3296e356ef2f330fa9f82718addf9073a0b2d8b0
maticarugno <mati.carugno>
2020-05-14 22:30:35 -0300

    Agrego tests de medios de pago

--- src/test/java/EgresosTests.java
+++ src/test/java/EgresosTests.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import mediosDePago.Efectivo;
+import mediosDePago.IdentificadorNullException;
+import mediosDePago.MedioDePago;
+import mediosDePago.TarjetaCredito;
+import org.junit.Test;
 
 public class EgresosTests {
+    @Test
+    public void crearTarjetaCreditoCorrecta() {
+        MedioDePago visa = new TarjetaCredito("visa", Long.valueOf("1234567890987654"));
+    }
+
+    @Test (expected = IdentificadorNullException.class)
+    public void elIdentificadorDelMedioPagoNoPuedeSerNull(){
+        MedioDePago pagoFacil = new Efectivo("pagoFacil",null);
+    }
 }
