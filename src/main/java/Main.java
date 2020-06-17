aeb6f2401c43e3ae832207df7dd2712fcdbd9d50
maticarugno <mati.carugno>
2020-06-16 22:19:40 -0300

    Agrego la clase Main y el plugin de maven para crear el .jar

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import mediosDePago.MedioDePago;
+import mediosDePago.TarjetaCredito;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+    }
+}
