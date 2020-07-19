bc04926dcbb1458269dd6a9ce5397c4b778c157c
Aurelia Pilipiak <aureliapilipiak@gmail.com>
2020-07-19 14:33:06 -0300

    Saco al proveedor del egreso, modifico métodos de validación

--- src/main/java/egreso/Egreso.java
+++ src/main/java/egreso/Egreso.java
@@ -2,55 +2,51 @@ package egreso;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import mediosDePago.MedioDePago;
 import presupuesto.Presupuesto;
 import proveedor.Moneda;
-import proveedor.Proveedor;
 import usuario.Usuario;
 
 public class Egreso
 {
     private List<DocComercial> documentosComerciales = new ArrayList<>();
     private MedioDePago medioDePago;
-    private Proveedor proveedor;
     private List<Item> items = new ArrayList<>();
     private Date fecha;
     private List<Presupuesto> presupuestos = new ArrayList<>();
-    private  boolean requierePresupuesto = true;
+    private boolean requierePresupuesto = true;
     private List<Usuario> revisores = new ArrayList<>();
     private Moneda moneda;
-
+    private CriterioCompra criterioDeSeleccion = CriterioMenorValor.getInstance();
     private static final int presupuestosRequeridos = 3;
 
     public BigDecimal totalEgreso()
     {
         BigDecimal total = new BigDecimal("0");
+        BigDecimal totalFinal = items.stream()
+                .map(x -> total.add(x.precioTotal()))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        for( int i=0; i < items.size(); i++)
-        {
-            total=total.add(items.get(i).precioTotal());
-        }
-        return total;
+        return totalFinal;
     }
 
-    public Egreso(List<DocComercial> unosDC, MedioDePago unMedioDePago, Proveedor proveedor, List<Item> unosItems, Date unaFecha, Moneda moneda)
+    public Egreso(List<DocComercial> unosDC, MedioDePago unMedioDePago, List<Item> unosItems, Date unaFecha, Moneda moneda)
     {
         this.documentosComerciales = unosDC;
         this.medioDePago = unMedioDePago;
-        this.proveedor = proveedor;
         this.items = unosItems;
         this.fecha = unaFecha;
         this.moneda = moneda;
         RepositorioDeEgresos.getInstance().agregarEgresosPendientes(this);
     }
 
-    public Egreso(List<DocComercial> unosDC, MedioDePago unMedioDePago, Proveedor proveedor, List<Item> unosItems, Date unaFecha, boolean requierePresupuesto, Moneda moneda)
+    public Egreso(List<DocComercial> unosDC, MedioDePago unMedioDePago, List<Item> unosItems, Date unaFecha, boolean requierePresupuesto, Moneda moneda)
     {
         this.documentosComerciales = unosDC;
         this.medioDePago = unMedioDePago;
-        this.proveedor = proveedor;
         this.items = unosItems;
         this.fecha = unaFecha;
         this.requierePresupuesto = requierePresupuesto;
@@ -66,21 +62,36 @@ public class Egreso
 
     public boolean esValido()
     {
+        return this.cumplePresupuestosMinimos() && //cantidad mínima de presupuestos requeridos
+                this.estaBasadoEnUnPresupuesto() &&
+                this.cumpleCriterioSeleccion();
+    }/* si en un futuro hay que hacer validaciones sin presupuesto, se pone un IF de si requiere o no, y lo sacamos
+        de cada una de estas validaciones. */
+    public boolean cumplePresupuestosMinimos()
+    {
         if(requierePresupuesto)
         {
-            if(presupuestos.size() >= presupuestosRequeridos) //validación "A"
-            {
-                if(validarProveedor()) //validación "C"
-                {
-                    for (Presupuesto presupuesto : presupuestos) { //validación "B"
-                        if (presupuesto.getDetalle().equals(items))
-                            return true;
-                    }
-                }
-            }
-            return false;
+            return presupuestos.size() >= presupuestosRequeridos;
         }
-        else return true;
+        return true;
+    }
+
+    public boolean estaBasadoEnUnPresupuesto()
+    {
+        if(requierePresupuesto)
+        {
+            return presupuestos.stream().anyMatch(x -> x.getDetalle().equals(items));
+        }
+        return true;
+    }
+
+    public boolean cumpleCriterioSeleccion()
+    {
+        if(requierePresupuesto)
+        {
+            return presupuestos.stream().anyMatch(x -> this.getCriterioDeSeleccion().presupuestosQueCumplen(presupuestos).contains(x));
+        }
+        return true;
     }
 
     public List<Usuario> getRevisores()
@@ -93,34 +104,9 @@ public class Egreso
         revisores.add(usuario);
     }
 
-    public boolean validarProveedor(){
-        BigDecimal menorValorPresupuesto = this.presupuestoMenorValor();
-        BigDecimal mayorValorPresupuesto = this.presupuestoMayorValor();
-        return (proveedor.getMenorValor() && this.totalEgreso().compareTo(menorValorPresupuesto) == 0) ||
-                (!proveedor.getMenorValor() && this.totalEgreso().compareTo(mayorValorPresupuesto) == 0);
-    }
-
-    public BigDecimal presupuestoMenorValor(){
-        BigDecimal valor = presupuestos.get(0).totalPresupuesto();
-        for (int i = 0; i < presupuestos.size(); i++){
-            if(valor.compareTo(presupuestos.get(i).totalPresupuesto()) == 1){
-                valor = presupuestos.get(i).totalPresupuesto();
-            }
-        }
-        return valor;
-    }
-
-    public BigDecimal presupuestoMayorValor(){
-        BigDecimal valor = new BigDecimal("0");
-        for (int i = 0; i < presupuestos.size(); i++){
-            if(valor.compareTo(presupuestos.get(i).totalPresupuesto()) == -1){
-                valor = presupuestos.get(i).totalPresupuesto();
-            }
-        }
-        return valor;
-    }
-
     public List<Item> getItems() {
         return items;
     }
+
+    public CriterioCompra getCriterioDeSeleccion() { return criterioDeSeleccion; }
 }
