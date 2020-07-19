fba23393a2e9f251350a91437d222ab215dfb3c9
Aurelia Pilipiak <aureliapilipiak@gmail.com>
2020-07-19 14:31:45 -0300

    Saco al proveedor del presupuesto

--- src/main/java/presupuesto/Presupuesto.java
+++ src/main/java/presupuesto/Presupuesto.java
@@ -5,6 +5,8 @@ import egreso.Egreso;
 import egreso.Item;
 import org.apache.commons.lang3.Validate;
 import proveedor.Moneda;
+import proveedor.Proveedor;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,22 +16,23 @@ public class Presupuesto {
     private List<DocComercial> documentosComerciales = new ArrayList<>();
     private Egreso egreso;
     private Moneda moneda;
+    private Proveedor proveedor;
 
-    public Presupuesto(List<Item> detalle, List<DocComercial> documentosComerciales, Egreso egreso, Moneda moneda){
+    public Presupuesto(List<Item> detalle, List<DocComercial> documentosComerciales, Egreso egreso, Moneda moneda, Proveedor proveedor){
         Validate.notNull(egreso, "El egreso no puede ser nulo");
         this.detalle = detalle;
         this.documentosComerciales = documentosComerciales;
         this.egreso = egreso;
         this.moneda = moneda;
+        this.proveedor = proveedor;
     }
 
     public BigDecimal totalPresupuesto() {
 
         BigDecimal total = new BigDecimal("0");
 
-        for( int i=0; i < detalle.size(); i++)
-        {
-            total=total.add(detalle.get(i).precioTotal());
+        for (Item item : detalle) {
+            total = total.add(item.precioTotal());
         }
         return total;
     }
