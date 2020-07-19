cdd1ba82f6b640b1794a57cf9ed4474b8f9fc40d
Aurelia Pilipiak <aureliapilipiak@gmail.com>
2020-07-19 14:33:39 -0300

    Saco al criterio del proveedor

--- src/main/java/proveedor/Proveedor.java
+++ src/main/java/proveedor/Proveedor.java
@@ -5,17 +5,11 @@ public class Proveedor {
     private String nombre;
     private String documento;
     private DireccionPostal direccionPostal;
-    private Boolean menorValor;
 
-    public Proveedor(String nombre, String documento, DireccionPostal direccionPostal, boolean menorValor){
+    public Proveedor(String nombre, String documento, DireccionPostal direccionPostal){
         this.nombre = nombre;
         this.documento = documento;
         this.direccionPostal = direccionPostal;
-        this.menorValor = menorValor;
-    }
-
-    public Boolean getMenorValor() {
-        return menorValor;
     }
 }
 
