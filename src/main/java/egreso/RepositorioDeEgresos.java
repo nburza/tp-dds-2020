e6ee3a9c6de892910cc86937d2f12b38d5808e87
nburza <nicoburza_96@hotmail.com>
2020-09-24 23:10:25 -0300

    modifico repositorio de egresos para heredar del generico

--- src/main/java/egreso/RepositorioDeEgresos.java
+++ src/main/java/egreso/RepositorioDeEgresos.java
@@ -1,28 +1,20 @@
 package egreso;
 
+import persistencia.RepositorioGenerico;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepositorioDeEgresos{
+public class RepositorioDeEgresos extends RepositorioGenerico<Egreso> {
 
-    private List<Egreso> egresos = new ArrayList<>();
     private static final RepositorioDeEgresos instance = new RepositorioDeEgresos();
 
     public static RepositorioDeEgresos getInstance(){return  instance;}
 
     private RepositorioDeEgresos(){ }
 
-    public void agregarEgresos(Egreso unEgreso)
-    {
-        egresos.add(unEgreso);
-    }
-
-    public void eliminarEgresos(Egreso unEgreso)
-    {
-        egresos.remove(unEgreso);
-    }
-
-    public List<Egreso> getEgresos() {
-        return egresos;
+    @Override
+    protected Class<Egreso> getClase() {
+        return Egreso.class;
     }
 }
